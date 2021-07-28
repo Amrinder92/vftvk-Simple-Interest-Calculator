a999a79de1bf896ab3c9d23a385e2521291ce447
amrinder <amrinder.rai@gmail.com>
2021-07-28 13:23:50 +0000

    WIP

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute(){
+    var principal = document.getElementById("principal").Value;
+if (principal =="" || principal <=0){
+    alert("Enter a positive number");
+    document.getElementById("principal").focus();
+    return;
 }
-        
+var rate = document.getElementById("rate").Value;
+var years =document.getElementById("years").Value;
+var interest = principal * years * rate / 100;
+var dateNow = new Date();
+var yearNow = parseInt(dateNow.getFullYear()) + parseInt(years);
+var resultDisplay = document.getElementById("result");
+resultDisplay.innerHTML = "If you deposit" + "<span class='highlight'>" + principal+ "</span>." + ", <br> at an interest of "+ "<span class='highlight'>"+rate+ "</span>%."+ "<br> You will recieve an amount of" + "<span class='highlight'>" + interest + "</span>"+", <br> in the year"+"<span class='highlight'>" + yearNow + "</span>";
+}
+function updateRate(){
+    var rateval = document.getElementById("rate").Value;
+    document.getElementById("rate_val").innerText = rateval;
+}
+
+
+
+
+
+
+
+
